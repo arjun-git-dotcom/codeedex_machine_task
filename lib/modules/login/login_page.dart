b721ed55a67925f28f337fdcc9c43bd3326a4e11
arjun-git-dotcom <you@example.com>
2026-01-17 17:51:45 +0530

    Implemented  login page

--- lib/modules/login/login_page.dart
+++ lib/modules/login/login_page.dart
@@ -7,72 +7,142 @@ class LoginPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+
+
+
+
+
     return ChangeNotifierProvider(
       create: (_) => LoginProvider(),
       child: Scaffold(
         body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Consumer<LoginProvider>(
-              builder: (context, provider, _) {
-                return Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    const Text(
-                      "Login",
-                      style: TextStyle(
-                        fontSize: 28,
-                        fontWeight: FontWeight.bold,
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 16),
+              child: Consumer<LoginProvider>(
+                builder: (context, provider, _) {
+                  return Column(
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [
+             
+                      ClipRRect(
+                        borderRadius: BorderRadius.circular(12),
+                        child: Image.asset(
+                          "assets/Frame 427320120.png",
+                          height: 220,
+                          fit: BoxFit.cover,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 24),
+                      const SizedBox(height: 24),
 
-                    TextField(
-                      controller: provider.emailController,
-                      decoration: const InputDecoration(
-                        labelText: "Email / Username",
-                        border: OutlineInputBorder(),
+             
+                      const Text(
+                        "Login",
+                        style: TextStyle(
+                          fontSize: 28,
+                          fontWeight: FontWeight.bold,
+                        ),
                       ),
-                    ),
-                    const SizedBox(height: 16),
+                      const SizedBox(height: 24),
 
-                    TextField(
-                      controller: provider.passwordController,
-                      obscureText: true,
-                      decoration: const InputDecoration(
-                        labelText: "Password",
-                        border: OutlineInputBorder(),
+                 
+                      TextField(
+                        controller: provider.emailController,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0xFFF2F2F2),
+                          labelText: "Email Address",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
+                          ),
+                        ),
                       ),
-                    ),
+                      const SizedBox(height: 16),
 
-                    if (provider.errorMessage != null) ...[
-                      const SizedBox(height: 12),
-                      Text(
-                        provider.errorMessage!,
-                        style: const TextStyle(color: Colors.red),
+                  
+                      TextField(
+                        controller: provider.passwordController,
+                        obscureText: !provider.isPasswordVisible,
+                        decoration: InputDecoration(
+                          filled: true,
+                          fillColor: const Color(0xFFF2F2F2),
+                          labelText: "Password",
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(12),
+                            borderSide: BorderSide.none,
+                          ),
+                          suffixIcon: IconButton(
+                            icon: Icon(provider.isPasswordVisible
+                                ? Icons.visibility
+                                : Icons.visibility_off),
+                            onPressed: provider.togglePasswordVisibility,
+                          ),
+                        ),
                       ),
+                      const SizedBox(height: 8),
+
+              
+                      Align(
+                        alignment: Alignment.centerRight,
+                        child: TextButton(
+                          onPressed: () {},
+                          child: const Text("Forgot password?"),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+
+                 
+                      SizedBox(
+                        height: 48,
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFF7A3E12),
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(12),
+                            ),
+                          ),
+                          onPressed: provider.isLoading
+                              ? null
+                              : () => provider.login(context),
+                          child: provider.isLoading
+                              ? const CircularProgressIndicator(
+                                  color: Colors.white,
+                                )
+                              : const Text("Login",style: TextStyle(color: Colors.white,fontSize: 20),),
+                        ),
+                      ),
+                      const SizedBox(height: 16),
+
+                  
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text("Don't have an account? "),
+                          GestureDetector(
+                            onTap: () {},
+                            child: const Text(
+                              "Sign Up",
+                              style: TextStyle(
+                                color: Colors.blue,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          )
+                        ],
+                      ),
+                      if (provider.errorMessage != null) ...[
+                        const SizedBox(height: 12),
+                        Text(
+                          provider.errorMessage!,
+                          style: const TextStyle(color: Colors.red),
+                          textAlign: TextAlign.center,
+                        ),
+                      ],
                     ],
-
-                    const SizedBox(height: 24),
-
-                    SizedBox(
-                      width: double.infinity,
-                      height: 48,
-                      child: ElevatedButton(
-                        onPressed: provider.isLoading
-                            ? null
-                            : () => provider.login(context),
-                        child: provider.isLoading
-                            ? const CircularProgressIndicator(
-                                color: Colors.white,
-                              )
-                            : const Text("Login"),
-                      ),
-                    ),
-                  ],
-                );
-              },
+                  );
+                },
+              ),
             ),
           ),
         ),
--- lib/modules/login/login_provider.dart
+++ lib/modules/login/login_provider.dart
@@ -1,12 +1,19 @@
 import 'package:flutter/material.dart';
 
 class LoginProvider extends ChangeNotifier {
+  bool isPasswordVisible = false;
   final emailController = TextEditingController();
   final passwordController = TextEditingController();
-
   bool isLoading = false;
   String? errorMessage;
 
+
+
+
+void togglePasswordVisibility() {
+    isPasswordVisible = !isPasswordVisible;
+    notifyListeners();
+  }
   void login(BuildContext context) {
     final email = emailController.text.trim();
     final password = passwordController.text.trim();
@@ -22,8 +29,7 @@ class LoginProvider extends ChangeNotifier {
     notifyListeners();
 
     Future.delayed(const Duration(seconds: 1), () {
-      if (email == "mobile@alisonsgroup.com" &&
-          password == "12345678") {
+      if (email == "mobile@alisonsgroup.com" && password == "12345678") {
         isLoading = false;
         notifyListeners();
         Navigator.pushReplacementNamed(context, '/home');
